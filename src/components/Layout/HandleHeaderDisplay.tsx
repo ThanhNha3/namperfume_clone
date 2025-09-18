becb052bfe3ce8cfb3fbdab8a21c1779aeabc799
Thanh Nha <nhantpc05188@fpt.edu.vn>
2025-09-19 01:04:07 +0700

    feat(src/constants/): create WINDOW_SIZES const for break points and use it in HandleHeaderDisplay.tsx

--- src/components/Layout/HandleHeaderDisplay.tsx
+++ src/components/Layout/HandleHeaderDisplay.tsx
@@ -4,13 +4,14 @@ import React from "react";
 import { useWindowSize } from "@/hooks/useWindowSize";
 import Header from "./Header";
 import HeaderMobile from "./HeaderMobile";
+import { WINDOW_SIZES } from "@/constants/common";
 
 export default function HandleHeaderDisplay() {
     const width = useWindowSize();
 
     return (
         <React.Fragment>
-            {width >= 1024 ? <Header /> : <HeaderMobile />}
+            {width >= WINDOW_SIZES.laptop ? <Header /> : <HeaderMobile />}
         </React.Fragment>
     );
 }
---
+++ src/constants/common.ts
@@ -0,0 +1,9 @@
+// Define common constants used across the application
+
+// Screen size breakpoints in pixels
+export const WINDOW_SIZES = {
+    mobile: 640,
+    tablet: 768,
+    laptop: 1024,
+    desktop: 1280,
+} as const;
