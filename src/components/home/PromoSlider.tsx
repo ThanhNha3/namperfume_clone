55bb9c9c0852a8faa7a0cdfd7aa5e85c8c1e038f
Thanh Nha <nhantpc05188@fpt.edu.vn>
2025-09-22 23:41:57 +0700

    update(promo_slider.ts): change name to camelCase style

--- src/components/home/PromoSlider.tsx
+++ src/components/home/PromoSlider.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { motion, AnimatePresence } from "framer-motion";
-import { PromoSliderProps } from "@/types/promo_slider";
+import { PromoSliderProps } from "@/types/promoSlider";
 
 export function PromoSlider({ 
   isShowIndicator = true, 
